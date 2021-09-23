03acebd7b4f87bb4bd25f9979a713400a9b4efa8
John Rinehart <john.rinehart@ardanlabs.com>
2021-09-23 15:39:49 +0300

    feat(atkuila): remove Nix VM/VPS (unused) and the associated block storage
    fix: change the terraform JohnOS.iso to point to the most recent hydra build

--- vultr.tf
+++ vultr.tf
@@ -7,7 +7,7 @@ provider "vultr" {
 
 # TODO: Support case open about the below which fails with 404 server not found (but server is alive)
 resource vultr_iso_private "john_os" {
-  url = "https://hydra.johnrinehart.dev/build/20/download/1/nixos.iso"
+  url = "https://hydra.johnrinehart.dev/build/25/download/1/nixos.iso"
 }
 
 resource "vultr_instance" "CloudNix" {
@@ -20,13 +20,4 @@ resource "vultr_instance" "MarinaBrave" {
   region="ams"
   plan = "vhf-1c-1gb"
   enable_ipv6 = true
-}
-
-resource "vultr_instance" "Nix" {
-  region = "ewr"
-  plan = "vc2-1c-1gb"
-  enable_ipv6 = true
-  iso_id = vultr_iso_private.john_os.id
-
-  label = "Nix"
 }
