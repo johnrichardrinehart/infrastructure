7850511747c9f54679343d68579cded87c4d8d17
John Rinehart <johnrichardrinehart@gmail.com>
2023-04-23 11:54:21 +0100

    chore: bump provider versions

--- main.tf
+++ main.tf
@@ -2,17 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.60.0"
+      version = "4.64.0"
     }
 
     google = {
       source  = "hashicorp/google"
-      version = "3.85.0"
+      version = "4.62.1"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.62.1"
     }
 
     b2 = {
       source  = "Backblaze/b2"
-      version = "0.7.0"
+      version = "0.8.4"
     }
   }
 
