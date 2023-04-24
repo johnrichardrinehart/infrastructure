4e03c860954908292b554ff9593fd0ee1bc733f6
John Rinehart <johnrichardrinehart@gmail.com>
2023-04-25 00:10:19 +0100

    feat: add Backblaze provisioning keys .env.sh

--- .env.sh
+++ .env.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 export TF_VAR_cloudflare_api_token=$(gcloud secrets versions access latest --secret=johnrinehart-dot-dev-cloudflare-api-token --project=webbie-295322)
+export B2_APPLICATION_KEY_ID=$(gcloud secrets versions access latest --secret=backblaze-provisioner-key-id --project=webbie-295322)
+export B2_APPLICATION_KEY=$(gcloud secrets versions access latest --secret=backblaze-provisioner-key --project=webbie-295322)
