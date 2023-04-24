8ef1b95856f030fdb079c8089fbee969a2bd0c3a
John Rinehart <johnrichardrinehart@gmail.com>
2023-04-25 00:12:10 +0100

    fix: upload GCE image regardles of pre-existing
    note: best would be to version the image so that it's unique for every
    build. This shouldn't be too hard.

--- process-and-upload-gce.sh
+++ process-and-upload-gce.sh
@@ -6,7 +6,7 @@ img_name=${IMAGE_NAME:-$(basename "$img_path")}
 img_id=$(echo "$img_name" | sed 's|.raw.tar.gz$||;s|\.|-|g;s|_|-|g')
 img_family=$(echo "$img_id" | cut -d - -f1-4)
 
-if ! gsutil ls "gs://${BUCKET_NAME}/$img_name"; then
+#if ! gsutil ls "gs://${BUCKET_NAME}/$img_name"; then
   gsutil cp "$img_path" "gs://${BUCKET_NAME}/$img_name"
   gsutil acl ch -u AllUsers:R "gs://${BUCKET_NAME}/$img_name"
-fi
+#fi
